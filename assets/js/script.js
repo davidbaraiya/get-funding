7ef5bc9ef8adba670664cc222da4beed8e1d6bbb
davidbaraiya <dineshbaraiya18@gmail.com>
2023-08-11 18:20:06 +0530

    form changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,27 @@ document.addEventListener("DOMContentLoaded", function () {
   var headerHeight = header.offsetHeight;
   wrapper.style.marginTop = headerHeight + "px";
 });
+
+// Get all elements with class 'fileInput'
+var fileInputs = document.querySelectorAll(".fileInput");
+
+// Loop through each 'fileInput' element
+fileInputs.forEach(function (fileInput) {
+  fileInput.addEventListener("change", function () {
+    var numFiles = this.files.length;
+    var parent = this.closest(".input-file");
+
+    // Remove existing 'ins' elements
+    var existingIns = parent.querySelectorAll("ins");
+    existingIns.forEach(function (ins) {
+      ins.remove();
+    });
+
+    // Loop through each selected file and add its name as 'ins' element
+    for (var i = 0; i < numFiles; i++) {
+      var insElement = document.createElement("ins");
+      insElement.textContent = this.files[i].name;
+      parent.appendChild(insElement);
+    }
+  });
+});
